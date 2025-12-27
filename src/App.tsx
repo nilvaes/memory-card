ef2da07f5217eefcd1f1fe9724bdcd948a2b3a00
Ömer <savas.qrx@gmail.com>
2025-12-27 16:07:03 +0100

    put all the logic back to App, fix later

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,150 @@
-import Cards from "./components/Cards";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 import GameHeader from "./components/GameHeader";
-import { useGame } from "./hooks/useGame";
+import WinMessage from "./components/WinMessage";
+
+export type CardType = {
+  index: number;
+  value: string;
+  isFlipped: boolean;
+  isMatched: boolean;
+};
+
+const cardValues: string[] = [
+  "drawing.png",
+  "helmet.png",
+  "shoes.png",
+  "stones.png",
+  "tea.png",
+  "tree.png",
+  "writing.png",
+  "yelpaze.png",
+  "drawing.png",
+  "helmet.png",
+  "shoes.png",
+  "stones.png",
+  "tea.png",
+  "tree.png",
+  "writing.png",
+  "yelpaze.png",
+];
 
 function App() {
-  const { cards, score, moves, startGame, handleCardClick } = useGame();
+  const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [score, setScore] = useState(0);
+  const [moves, setMoves] = useState(0);
+  const [isLocked, setIsLocked] = useState(false);
 
+  const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  };
+  const initializeGame = () => {
+    // shuffle the cards
+    const shuffled = shuffleArray(cardValues);
+
+    const finalCards = shuffled.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+
+    setCards(finalCards);
+    setMoves(0);
+    setScore(0);
+    setMatchedCards([]);
+    setFlippedCards([]);
+    setIsLocked(false);
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    initializeGame();
+  }, []);
+
+  const handleCardClick = (card: CardType) => {
+    // dont allow clicking if already flipped, or matched
+    if (
+      card.isFlipped ||
+      card.isMatched ||
+      isLocked ||
+      flippedCards.length === 2
+    ) {
+      return;
+    }
+    // update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+
+    const newFlippedCards = [...flippedCards, card.id];
+    setFlippedCards(newFlippedCards);
+
+    // check for match if two cards are flipped
+    if (flippedCards.length === 1) {
+      setIsLocked(true);
+      const firstCard = cards[flippedCards[0]];
+      if (firstCard.value === card.value) {
+        setTimeout(() => {
+          setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
+          setScore((prev) => prev + 1);
+
+          setCards((prev) =>
+            prev.map((c) => {
+              if (c.id === card.id || c.id === firstCard.id) {
+                return { ...c, isMatched: true };
+              } else {
+                return c;
+              }
+            })
+          );
+          setFlippedCards([]);
+          setIsLocked(false);
+        });
+      } else {
+        // flip back card 1, card 2
+        setTimeout(() => {
+          const flippedBackCard = newCards.map((c) => {
+            if (newFlippedCards.includes(c.id) || c.id === card.id) {
+              return { ...c, isFlipped: false };
+            } else {
+              return c;
+            }
+          });
+          setCards(flippedBackCard);
+
+          setFlippedCards([]);
+          setIsLocked(false);
+        }, 500);
+      }
+
+      setMoves((prev) => prev + 1);
+    }
+  };
+
+  const isGameComplete = matchedCards.length === cardValues.length;
   return (
-    <div className=" flex flex-col items-center justify-center min-h-screen bg-slate-800">
-      <GameHeader score={score} moves={moves} onStart={startGame} />
-      <Cards cards={cards} onClick={handleCardClick} />
+    <div className="app">
+      <GameHeader score={score} moves={moves} onReset={initializeGame} />
+      {isGameComplete && <WinMessage moves={moves} />}
+      <div className="cards-grid">
+        {cards.map((card) => (
+          <Card card={card} onClick={handleCardClick} />
+        ))}
+      </div>
     </div>
   );
 }
