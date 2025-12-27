9890c528dd0876908edd8b481b3388d1fbf7ddb4
Ömer <savas.qrx@gmail.com>
2025-12-27 18:49:50 +0100

    fix type of cardValues, import logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,148 +1,123 @@
-import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import GameHeader from "./components/GameHeader";
 import WinMessage from "./components/WinMessage";
+import { useGame, type CardType } from "./hooks/useGame";
 
-export type CardType = {
-  index: number;
-  value: string;
-  isFlipped: boolean;
-  isMatched: boolean;
-};
-
-const cardValues: string[] = [
-  "drawing.png",
-  "helmet.png",
-  "shoes.png",
-  "stones.png",
-  "tea.png",
-  "tree.png",
-  "writing.png",
-  "yelpaze.png",
-  "drawing.png",
-  "helmet.png",
-  "shoes.png",
-  "stones.png",
-  "tea.png",
-  "tree.png",
-  "writing.png",
-  "yelpaze.png",
+const cardValues: CardType[] = [
+  {
+    id: 1,
+    value: "drawing.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 2,
+    value: "helmet.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 3,
+    value: "shoes.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 4,
+    value: "stones.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 5,
+    value: "tea.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 6,
+    value: "tree.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 7,
+    value: "writing.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 8,
+    value: "yelpaze.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 9,
+    value: "drawing.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 10,
+    value: "helmet.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 11,
+    value: "shoes.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 12,
+    value: "stones.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 13,
+    value: "tea.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 14,
+    value: "tree.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 15,
+    value: "writing.png",
+    isFlipped: false,
+    isMatched: false,
+  },
+  {
+    id: 16,
+    value: "yelpaze.png",
+    isFlipped: false,
+    isMatched: false,
+  },
 ];
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [flippedCards, setFlippedCards] = useState([]);
-  const [matchedCards, setMatchedCards] = useState([]);
-  const [score, setScore] = useState(0);
-  const [moves, setMoves] = useState(0);
-  const [isLocked, setIsLocked] = useState(false);
-
-  const shuffleArray = (array) => {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-  };
-  const initializeGame = () => {
-    // shuffle the cards
-    const shuffled = shuffleArray(cardValues);
-
-    const finalCards = shuffled.map((value, index) => ({
-      id: index,
-      value,
-      isFlipped: false,
-      isMatched: false,
-    }));
-
-    setCards(finalCards);
-    setMoves(0);
-    setScore(0);
-    setMatchedCards([]);
-    setFlippedCards([]);
-    setIsLocked(false);
-  };
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    initializeGame();
-  }, []);
-
-  const handleCardClick = (card: CardType) => {
-    // dont allow clicking if already flipped, or matched
-    if (
-      card.isFlipped ||
-      card.isMatched ||
-      isLocked ||
-      flippedCards.length === 2
-    ) {
-      return;
-    }
-    // update card flipped state
-    const newCards = cards.map((c) => {
-      if (c.id === card.id) {
-        return { ...c, isFlipped: true };
-      } else {
-        return c;
-      }
-    });
-
-    setCards(newCards);
-
-    const newFlippedCards = [...flippedCards, card.id];
-    setFlippedCards(newFlippedCards);
-
-    // check for match if two cards are flipped
-    if (flippedCards.length === 1) {
-      setIsLocked(true);
-      const firstCard = cards[flippedCards[0]];
-      if (firstCard.value === card.value) {
-        setTimeout(() => {
-          setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
-          setScore((prev) => prev + 1);
-
-          setCards((prev) =>
-            prev.map((c) => {
-              if (c.id === card.id || c.id === firstCard.id) {
-                return { ...c, isMatched: true };
-              } else {
-                return c;
-              }
-            })
-          );
-          setFlippedCards([]);
-          setIsLocked(false);
-        });
-      } else {
-        // flip back card 1, card 2
-        setTimeout(() => {
-          const flippedBackCard = newCards.map((c) => {
-            if (newFlippedCards.includes(c.id) || c.id === card.id) {
-              return { ...c, isFlipped: false };
-            } else {
-              return c;
-            }
-          });
-          setCards(flippedBackCard);
-
-          setFlippedCards([]);
-          setIsLocked(false);
-        }, 500);
-      }
-
-      setMoves((prev) => prev + 1);
-    }
-  };
-
-  const isGameComplete = matchedCards.length === cardValues.length;
+  const {
+    cards,
+    moves,
+    score,
+    isGameComplete,
+    handleCardClick,
+    initializeGame,
+  } = useGame(cardValues);
   return (
     <div className="app">
       <GameHeader score={score} moves={moves} onReset={initializeGame} />
       {isGameComplete && <WinMessage moves={moves} />}
       <div className="cards-grid">
         {cards.map((card) => (
-          <Card card={card} onClick={handleCardClick} />
+          <Card card={card} onClick={handleCardClick} key={card.id} />
         ))}
       </div>
     </div>
